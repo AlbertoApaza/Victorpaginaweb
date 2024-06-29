886ecd1e87f83069f26de583726567095061db23
Alberto Apaza <141537380+AlbertoApaza@users.noreply.github.com>
2024-06-29 17:42:45 -0500

    Add files via upload

---
+++ SitioWeb Don Italo/preolader.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const preloaderContainer = document.querySelector(".preloader-container");
+
+
+    setTimeout(function() {
+
+        preloaderContainer.style.display = "none";
+    }, 3000);
+});
